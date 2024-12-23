fbc7ca8090205a6bb3a4ee16a0e1e639133dcd41
Lobanokivan11 <lobanokivan11@gmail.com>
2024-12-23 16:05:11 +0300

    #

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
+sudo apt update
+sudo apt install git git-lfs
+sudo apt install openjdk-17
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install gradle
+sudo git lfs install
+git clone https://github.com/microg/GmsCore.git input
+cd input
 patch -t -s -p1 < ../gmscore.patch
